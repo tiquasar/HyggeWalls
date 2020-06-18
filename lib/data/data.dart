558c2ae312566ed3ef44b03c387439c82dbe7449
Ayush Anand <tiquasar@gmail.com>
2020-06-19 01:55:33 +0530

    Performance Optimizations

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,27 +1,27 @@
 import 'package:hyggewalls/models/category_model.dart';
 
-String apiKEY = "Enter the API key here";
+String apiKEY = "Enter the API KEY here";
 
 List<CategoryModel> getCategories() {
-  List<CategoryModel> categorys = new List();
+  List<CategoryModel> categories = new List();
   CategoryModel categoryModel = new CategoryModel();
 
   categoryModel.imgUrl =
   "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "4k Wallpapers";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   categoryModel.imgUrl =
   "https://images.pexels.com/photos/1109354/pexels-photo-1109354.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Abstract";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   categoryModel.imgUrl =
   "https://images.pexels.com/photos/889929/pexels-photo-889929.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Aquatic\nWildlife";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
 
@@ -30,33 +30,33 @@ List<CategoryModel> getCategories() {
   categoryModel.imgUrl =
       "https://images.pexels.com/photos/704320/pexels-photo-704320.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Wild Life";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   //
   categoryModel.imgUrl =
       "https://images.pexels.com/photos/1107717/pexels-photo-1107717.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Nature";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   //
   categoryModel.imgUrl =
       "https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "City";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   categoryModel.imgUrl =
   "https://images.pexels.com/photos/970089/pexels-photo-970089.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Minimal\nWallpapers";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   categoryModel.imgUrl =
   "https://images.pexels.com/photos/2882234/pexels-photo-2882234.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Road\nJournies";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   //
@@ -64,7 +64,7 @@ List<CategoryModel> getCategories() {
       "https://images.pexels.com/photos/3806690/pexels-photo-3806690.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260";
   categoryModel.categoryName = "Motivation";
 
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
 
@@ -73,22 +73,22 @@ List<CategoryModel> getCategories() {
   categoryModel.imgUrl =
       "https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Bikes";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   //
   categoryModel.imgUrl =
       "https://images.pexels.com/photos/2301226/pexels-photo-2301226.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Cars";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
   //
   categoryModel.imgUrl =
   "https://images.pexels.com/photos/545008/pexels-photo-545008.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   categoryModel.categoryName = "Street Art";
-  categorys.add(categoryModel);
+  categories.add(categoryModel);
   categoryModel = new CategoryModel();
 
-  return categorys;
+  return categories;
 }
